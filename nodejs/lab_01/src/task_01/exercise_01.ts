931fa71d48e189b6ed78b618e3272f81ec654c19
Sergey Kononenko <hackfeed@yandex.ru>
2020-09-07 15:27:06 +0300

    Complete exercise_01 of lab_01

--- nodejs/lab_01/src/task_01/exercise_01.ts
+++ nodejs/lab_01/src/task_01/exercise_01.ts
@@ -1,6 +1,7 @@
-import { Kid, KidUpdate } from "./interfaces";
+import { Kid } from "./interfaces";
 
-const kids: Kid[] = [];
+let kids: Kid[] = [];
+const vowels = "aeiouy";
 
 export const addKid = (surname: string, age: number): boolean => {
   const isExist = kids.filter((kid) => kid.surname === surname).length > 0;
@@ -25,14 +26,74 @@ export const getKid = (surname: string): Kid | undefined => {
   return kid;
 };
 
-export const updateKid = (surname: string, data: KidUpdate) => {
+export const updateKid = (surname: string, data: Kid): boolean => {
   const kid = getKid(surname);
 
   if (!kid) {
     return false;
   }
 
-  for (const key in Object.keys(data)) {
-    kid.get(key) = data[key];
+  kid.surname = data.surname;
+  kid.age = data.age;
+
+  return true;
+};
+
+export const deleteKid = (surname: string): boolean => {
+  const newKids = kids.filter((kid) => kid.surname === surname);
+
+  if (newKids === kids) {
+    return false;
   }
+
+  kids = newKids;
+
+  return true;
+};
+
+export const getAverageAge = (): number => {
+  const age = kids
+    .map((kid) => kid.age)
+    .reduce((total, age, index, array) => {
+      total += age;
+      if (index === array.length - 1) {
+        return total / array.length;
+      } else {
+        return total;
+      }
+    });
+
+  return age;
+};
+
+export const getEldestKid = (): Kid => {
+  const eldestKid = kids.reduce((eldest, kid) => (kid.age > eldest.age ? kid : eldest), kids[0]);
+
+  return eldestKid;
+};
+
+export const getKidsByAge = (ages: number[]): Kid[] => {
+  const agedKids = kids.filter((kid) => ages.includes(kid.age));
+
+  return agedKids;
+};
+
+export const getKidsBySurnameLetter = (letter: string): Kid[] => {
+  const letterKids = kids.filter((kid) =>
+    kid.surname.toLowerCase().startsWith(letter.toLowerCase())
+  );
+
+  return letterKids;
+};
+
+export const getKidsBySurnameLength = (len: number): Kid[] => {
+  const lenKids = kids.filter((kid) => kid.surname.length > len);
+
+  return lenKids;
+};
+
+export const getKidsByFirstVowelSurname = (): Kid[] => {
+  const vowelKids = kids.filter((kid) => vowels.includes(kid.surname[0].toLowerCase()));
+
+  return vowelKids;
 };
--- nodejs/lab_01/src/task_01/interfaces.ts
+++ nodejs/lab_01/src/task_01/interfaces.ts
@@ -2,8 +2,3 @@ export interface Kid {
   surname: string;
   age: number;
 }
-
-export interface KidUpdate {
-  surname?: string;
-  age?: number;
-}
