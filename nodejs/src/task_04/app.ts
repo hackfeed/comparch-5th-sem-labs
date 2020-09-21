9bc8c1b6a2b2d5165bcbc0cb48a9d8d84a88197c
Sergey Kononenko <hackfeed@yandex.ru>
2020-09-22 00:23:18 +0300

    Add styling

--- nodejs/src/task_04/app.ts
+++ nodejs/src/task_04/app.ts
@@ -3,6 +3,7 @@ import { router as ex02Router } from "./routes/exercise_02";
 import { router as ex03Router } from "./routes/exercise_03";
 import { router as ex04Router } from "./routes/exercise_04";
 import express from "express";
+import { join } from "path";
 import { urlencoded } from "body-parser";
 
 const app = express();
@@ -11,6 +12,7 @@ app.set("view engine", "pug");
 app.set("views", "views");
 
 app.use(urlencoded({ extended: false }));
+app.use(express.static(join(__dirname, "public")));
 
 app.get("/", (_: express.Request, res: express.Response) =>
   res.render("index", { pageTitle: "Main page" })
